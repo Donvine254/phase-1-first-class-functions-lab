9f973bcd47e55955ef8d2a8da380ecf402fb10a3
donvine mugendi <donvinemugendi@gmail.com>
2023-03-31 21:07:24 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const drivers = ["Antonia", "Nuru", "Amari", "Mo"];
 const returnFirstTwoDrivers = (drivers) => drivers.slice(0, 2);
 const returnLastTwoDrivers = () => drivers.slice(2);
 const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
-const createFareMultiplier = function (multiplier) {
+const createFareMultiplier = (multiplier)=> {
   return function (fare) {
     return fare * multiplier;
   };
